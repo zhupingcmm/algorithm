599c443219ae3bc00b9f2e8a9a21d6072a32bf62
pingzhu <ping.zhu@microfocus.com>
2022-06-25 07:59:17 +0800

    remove duplicate item

---
+++ src/main/java/com/mf/algorithm/RemoveDuplicates26.java
@@ -0,0 +1,22 @@
+package com.mf.algorithm;
+
+public class RemoveDuplicates26 {
+    public static void main(String[] args) {
+        RemoveDuplicates26 removeDuplicates = new RemoveDuplicates26();
+        removeDuplicates.removeDuplicates(new int[]{1,1,2});
+    }
+
+    public int removeDuplicates (int [] nums) {
+        if (nums == null||nums.length ==0) return 0;
+        int left = 0;
+        int right = 1;
+        while (right < nums.length) {
+            if (nums[left] != nums[right]) {
+                nums[left+1] = nums[right];
+                left++;
+            }
+            right++;
+        }
+        return left + 1;
+    }
+}
