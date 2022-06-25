8f76449ce94d87c544eeba3ad78416bea71b38be
pingzhu <ping.zhu@microfocus.com>
2022-06-25 08:58:57 +0800

    add merge

--- src/main/java/com/mf/algorithm/Merge88.java
+++ src/main/java/com/mf/algorithm/Merge88.java
@@ -3,11 +3,13 @@ package com.mf.algorithm;
 import java.util.Arrays;
 
 public class Merge88 {
+    public static void main(String[] args) {
+
+    }
     private void merge (int[] num1, int m, int [] num2, int n){
         for (int i = 0; i < n; i++) {
             num1[m + i] = num2[i];
         }
-
         Arrays.sort(num1);
     }
 }
