0c4a1802700efa28daa75f24d2f05c5b7a6bb7dc
pingzhu <ping.zhu@microfocus.com>
2022-07-01 11:57:37 +0800

    add change

--- src/main/java/com/mf/algorithm/RemoveDuplicates26.java
+++ src/main/java/com/mf/algorithm/RemoveDuplicates26.java
@@ -7,12 +7,12 @@ public class RemoveDuplicates26 {
     }
 
     public int removeDuplicates (int [] nums) {
-        if (nums == null||nums.length ==0) return 0;
+        if (nums == null || nums.length == 0) return 0;
         int left = 0;
         int right = 1;
         while (right < nums.length) {
             if (nums[left] != nums[right]) {
-                nums[left+1] = nums[right];
+                nums[left + 1] = nums[right];
                 left++;
             }
             right++;
---
+++ src/main/java/com/mf/algorithm/TreeNode.java
@@ -0,0 +1,13 @@
+package com.mf.algorithm;
+
+public class TreeNode {
+
+    int val;
+     TreeNode left;
+     TreeNode right;
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
