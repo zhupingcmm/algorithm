c40467fd52120aeb6ad286d7feac0be8aee65ebb
pingzhu <ping.zhu@microfocus.com>
2022-12-14 16:47:21 +0800

    change

--- src/main/java/com/mf/algorithm/LowestCommonAncestor235.java
+++ src/main/java/com/mf/algorithm/LowestCommonAncestor235.java
@@ -3,17 +3,16 @@ package com.mf.algorithm;
 public class LowestCommonAncestor235 {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-        if (root.val > p.val && root.val > q.val ) {
+        if (root == null) return root;
+        if (p.val < root.val && q.val < root.val) {
             TreeNode left = lowestCommonAncestor(root.left, p, q);
             if (left != null) return left;
         }
 
-        if (root.val < p.val && root.val < q.val) {
+        if (p.val > root.val && q.val > root.val) {
             TreeNode right = lowestCommonAncestor(root.right, p, q);
-            if (right != null) return right;
+            if (right != null) return  right;
         }
-
 
         return root;
     }
--- src/main/java/com/mf/algorithm/LowestCommonAncestor236.java
+++ src/main/java/com/mf/algorithm/LowestCommonAncestor236.java
@@ -2,16 +2,15 @@ package com.mf.algorithm;
 
 public class LowestCommonAncestor236 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
         if (root == null) return null;
         if (root == p || root == q) return root;
-
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
+
         if (left == null && right != null) return right;
         if (left != null && right == null) return left;
-        if (left == null && right == null) return null;
-
-        return root;
-
+        if (left != null && right != null) return root;
+        return null;
     }
 }
